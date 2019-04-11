537654c345abdccfadd4be43066dd514cbc3ff2f
Charles Daniels <cdaniels@fastmail.com>
2019-04-11 15:38:03 -0400

    add PARALLEL_OPTS

--- scripts/build_figures.sh
+++ scripts/build_figures.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
 
+# generate all data and figures
+
+# Environment variables:
+#
+# PARALLEL_OPTS:
+#
+# Added to calls to GNU parallel. For example, to distribute over a cluster of
+# machines: --sshloginfile ~/.parallel/sshloginfile  --sshdelay 0.1
+
 cd "$(dirname "$0")"
 cd ..
 
@@ -8,21 +17,23 @@ make -j4
 
 SIZE=10
 SEED=13245
-TICKS=500
+TICKS=64000
 
 mkdir -p out
 rm -f out/*
+WHERE="$(pwd)"
+sleep 1
 (
 for topo in mesh torus utorus; do
 	for P in 0.001 0.05 0.075 0.1 0.125 0.15 0.175 0.2 0.225 0.25 0.275 0.3 0.325 0.350 0.375 0.4 0.425 0.45 0.475 0.5 ; do
 		for behavior in DOR ADOR ; do
-			printf "python3 scripts/gengrid.py --size $SIZE \
+			printf "cd '$WHERE' ; python3 scripts/gengrid.py --size $SIZE \
 --topography $topo --P_inject $P --seed $SEED --behavior $behavior \
 --ticks $TICKS | ./src/nocsim 2>/dev/null | python3 scripts/evalstats.py \
 > 'out/$topo.$P.$behavior.out' 2> 'out/$topo.$P.$behavior.err'\n"
 		done
 	done
-done ) | parallel --bar
+done ) | parallel $PARALLEL_OPTS --bar
 
 cat $(ls -1 out/*.err | head -n1) > out/headers.tsv
 cat out/headers.tsv out/*.out > out/results.tsv
@@ -39,6 +50,6 @@ export GP_EXT=pdf
 
 ( for f in scripts/figures/*.sh ; do
 	printf "sh '$f'\n"
-done ) | parallel --bar
+done ) | parallel $PARALLEL_OPTS --bar
 
 pdfunite out/*.pdf out/all.pdf
