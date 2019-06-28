50984a8dcf32c6bd35d173377d55da8590056f9b
Charles Daniels <charles@cdaniels.net>
2019-06-28 09:25:26 -0400

    setup travis

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -11,15 +11,6 @@ if ! make ECFLAGS=-Werror ; then
 	PASS=NO
 fi
 
-echo '#### LINT #####################################################################'
-make clean
-make lint
-if [ "$(ikos-report --status-filter error -f csv src/nocsim.db | wc -l)" -gt 0 ] ; then
-	PASS=NO
-fi
-
-
-
 if [ "$PASS" = YES ] ; then
 	exit 0
 else
