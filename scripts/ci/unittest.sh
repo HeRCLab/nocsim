f10d91a36dc62c59ea6bebf1d00e44072c8bd593
Charles Daniels <charles@cdaniels.net>
2019-07-10 18:46:00 -0400

    unittests prints backtrace on segfault

--- scripts/ci/unittest.sh
+++ scripts/ci/unittest.sh
@@ -10,19 +10,23 @@ make clean
 make
 
 FAILED=0
+
+onfail () {
+	FAILED=$(expr $FAILED + 1)
+	echo "$f: FAIL"
+	echo '----- failing test output -----------------------------------------------------'
+	cat "$temp"
+}
+
 for f in scripts/ci/unittest/*.tcl ; do
 	temp="$(mktemp)"
 	if ! ./src/nocsim < "$f" > "$temp" 2>&1; then
-		FAILED=$(expr $FAILED + 1)
-		echo "$f: FAIL"
-		echo '----- failing test output -----------------------------------------------------'
-		cat "$temp"
+		onfail
+		echo '----- backtrace ---------------------------------------------------------------'
+		printf 'r < '"$f"'\nbt\nquit\ny\n' | gdb ./src/nocsim
 	elif grep "FAILED" < "$temp" > /dev/null 2>&1 ; then
-		FAILED=$(expr $FAILED + 1)
-		echo "$f: FAIL"
-		echo '----- failing test output -----------------------------------------------------'
-		cat "$temp"
-	else 
+		onfail
+	else
 		echo "$f: PASS"
 	fi
 	rm -f "$temp"
