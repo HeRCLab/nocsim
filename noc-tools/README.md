ee2a62e8446203d91eb6e4b132312e694ae9beda
Charles Daniels <charles@cdaniels.net>
2019-07-30 15:17:36 -0400

    setup build system

---
+++ noc-tools/README.md
@@ -0,0 +1,21 @@
+# `nocsim-gui`
+
+A simple GUI for interacting with `nocsim`. The `nocsim` code files are
+symlinked into this directory, and the GUI binary is built independently from
+the `nocsim` CLI binary.
+
+# Building the GUI
+
+`./configure`
+`make`
+
+Note that the `nocsim` GUI depends on [libAgar](http://libagar.org/) version
+1.6.0 or better. At time of writing, this version of libAgar does not have a
+stable release. SVN version r10336 is known to work correctly to build
+`nocsim-gui`.
+
+## Troubleshooting
+
+### `./nocsim-gui: symbol lookup error: ./nocsim-gui: undefined symbol: ag_get_named_ptr`
+
+libAgar is not in your `LD_LIBRARY_PATH`
---
+++ noc-tools/configure.lua
@@ -0,0 +1,70 @@
+-- Public domain
+--
+-- Do not edit!
+-- This file was generated from configure.in by BSDBuild 3.1.
+--
+-- To regenerate this file, get the latest BSDBuild release from
+-- http://bsdbuild.hypertriton.com/, and use the command:
+--
+--    $ cat configure.in | mkconfigure > configure
+--
+hdefs = {}
+mdefs = {}
+hdefs["ENABLE_NLS"] = nil
+hdefs["PREFIX"] = "/usr/local"
+hdefs["BINDIR"] = "/usr/local/bin"
+hdefs["LIBDIR"] = "/usr/local/lib"
+hdefs["MODULEDIR"] = "/usr/local/lib"
+hdefs["LIBEXECDIR"] = "/usr/local/libexec"
+hdefs["DATADIR"] = "/usr/local/share"
+hdefs["STATEDIR"] = "/usr/local/var"
+hdefs["SYSCONFDIR"] = "/usr/local/etc"
+hdefs["LOCALEDIR"] = "/usr/local/share/locale"
+hdefs["MANDIR"] = "/usr/local/man"
+hdefs["PACKAGE"] = "nocsim-gui"
+hdefs["VERSION"] = "2.0.0"
+hdefs["EXECSUFFIX"] = ""
+hdefs["HAVE_CC_WARNINGS"] = "yes"
+hdefs["HAVE_LONG_DOUBLE"] = "yes"
+hdefs["HAVE_LONG_LONG"] = "yes"
+hdefs["HAVE_CYGWIN"] = nil
+hdefs["HAVE_CC_MWINDOWS"] = nil
+hdefs["HAVE_CC_MCONSOLE"] = nil
+hdefs["HAVE_AGAR"] = "yes"
+hdefs["AGAR_CFLAGS"] = "-I/home/cad/.local/agar1.6//include/agar   -I/usr/include/freetype2 -I/usr/include/libpng16 -I/usr/include/uuid -I/usr/include/freetype2 -I/usr/include/libpng16        -I/usr/include/libpng16          "
+hdefs["AGAR_LIBS"] = "-L/home/cad/.local/agar1.6//lib -lag_gui -lag_core  -lfreetype -lfontconfig -lfreetype  -L/usr/local/lib -lGL -lX11 -lXinerama  -lm   -lpng16 -lpthread     -ldl    "
+hdefs["HAVE_TCL"] = "yes"
+hdefs["TCL_CFLAGS"] = "-I/usr/include/tcl8.6"
+hdefs["TCL_LIBS"] = "-ltcl8.6 -ltclstub8.6"
+mdefs["AGAR_CFLAGS"] = "-I/home/cad/.local/agar1.6//include/agar   -I/usr/include/freetype2 -I/usr/include/libpng16 -I/usr/include/uuid -I/usr/include/freetype2 -I/usr/include/libpng16        -I/usr/include/libpng16          "
+mdefs["HAVE_LD_NO_UNDEFINED"] = ""
+mdefs["DATADIR"] = "/usr/local/share"
+mdefs["PACKAGE"] = "nocsim-gui"
+mdefs["TCL_CFLAGS"] = "-I/usr/include/tcl8.6"
+mdefs["HAVE_CC_MCONSOLE"] = "no"
+mdefs["HAVE_CYGWIN"] = "no"
+mdefs["LIBS"] = " -ltcl"
+mdefs["HAVE_GETTEXT"] = "no"
+mdefs["CFLAGS"] = " -I/usr/include/tcl8.6"
+mdefs["PROG_GUI_FLAGS"] = ""
+mdefs["HAVE_LD_STATIC_LIBGCC"] = ""
+mdefs["LIBDIR"] = "/usr/local/lib"
+mdefs["MODULEDIR"] = "/usr/local/lib"
+mdefs["PROG_CLI_FLAGS"] = ""
+mdefs["SYSCONFDIR"] = "/usr/local/etc"
+mdefs["EXECSUFFIX"] = ""
+mdefs["HAVE_AGAR"] = "yes"
+mdefs["STATEDIR"] = "/usr/local/var"
+mdefs["HAVE_CC_MWINDOWS"] = "no"
+mdefs["HAVE_CC_WARNINGS"] = "yes"
+mdefs["CC"] = "/usr/bin/cc"
+mdefs["VERSION"] = "2.0.0"
+mdefs["LIBTOOLOPTS_SHARED"] = ""
+mdefs["LOCALEDIR"] = "/usr/local/share/locale"
+mdefs["AGAR_LIBS"] = "-L/home/cad/.local/agar1.6//lib -lag_gui -lag_core  -lfreetype -lfontconfig -lfreetype  -L/usr/local/lib -lGL -lX11 -lXinerama  -lm   -lpng16 -lpthread     -ldl    "
+mdefs["MANDIR"] = "/usr/local/man"
+mdefs["HAVE_TCL"] = "yes"
+mdefs["LIBEXECDIR"] = "/usr/local/libexec"
+mdefs["ENABLE_NLS"] = "no"
+mdefs["BINDIR"] = "/usr/local/bin"
+mdefs["TCL_LIBS"] = "-ltcl8.6 -ltclstub8.6"
